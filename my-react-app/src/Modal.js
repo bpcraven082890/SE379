f246c4129e336bdf88226f1879025e72d079fd5a
Ben Craven <bpcraven082890@gmail.com>
2024-04-02 19:05:53 -0400

    Adding JS App to React App

---
+++ my-react-app/src/Modal.js
@@ -0,0 +1,12 @@
+export const Modal = (props) => {
+    return (
+        <>
+        <div className="modal">
+            <p>Did you complete this task?</p>
+            <button onClick={()=>props.dismissModal()}className="btn btn-highlihgt">Cancel</button>
+            <button onClick={()=>props.dismissModal()}className="btn">Confirm</button>
+        </div>
+        <div onClick={()=>props.dismissModal()}className="backdrop" ></div>
+        </>
+    )
+}
